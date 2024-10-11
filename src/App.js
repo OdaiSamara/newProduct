28f69cbfe2982620f631d1159b9bd710a06ca967
OdaiSamara <166866058+OdaiSamara@users.noreply.github.com>
2024-10-11 06:00:58 -0700

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,11 +1,11 @@
 import Forms from "./component/Forms";
-import Hader from "./component/Hader";
+
 
 
 function App() {
   return (
     <div className="App">
-      <Hader />
+     
       <Forms/>
     </div>
   );
